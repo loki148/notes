5b787e7a396fe5c52d64905d719cc2cdd8607b82
loki148 <96773742+loki148@users.noreply.github.com>
2024-04-13 20:32:34 +0200

    Add new and create items & links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,12 @@ class ItemsController < ApplicationController
     def index
         @items = Item.all
     end
+
+    def new
+    end
+
+    def create
+        @item = Item.create(title: params[:name], expected_cost: params[:expected_cost], is_done: params[:is_done])
+        redirect_to @item
+    end
 end
